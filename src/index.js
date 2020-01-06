20b8a48a826625291012a80ef7fe4918bca2d332
jymma karina mogollon cardoza <jkmogollon.cp@gmail.com>
2020-01-06 18:52:05 -0500

    practice react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const hello = <h1>Hello Platzi</h1>;
+const element = React.createElement('h1', {}, 'Soy su hijo');
+const link = React.createElement('a', { href: 'https://platzi.com/clases/1548-react/18694-jsx2383/' }, 'Hola soy un link');
+const newElement = React.createElement('h1', {}, `Hola soy Karina`);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const name = `Karina`;
+const title = React.createElement('h1', {}, `Mi nombre es ${name}`);
+
+const jsx = <h1>Hello, {name} </h1>;
+
+//operadores
+const suma = <p>El resultado es: {3 + 5} </p>;
+
+
+const rest = () => 5 - 2;
+const resta = <p>El resultado es: {rest()} </p>;
+
+//contenedor
+const contenedor = (
+    <div>
+        <h1> Hola, soy Jymma Karina</h1>
+        <p>Soy Front-End</p>
+    </div>
+);
+
+const contTwo = React.createElement(
+
+    'div',
+
+    {},
+    React.createElement('h1',{}, 'Hola soy Jymma'),
+    React.createElement('p',{}, 'Soy front')
+
+)
+
+//false, null, undefined
+
+const expresion = <h1>Hello, {false} </h1>;
+
+
+
+
+const container = document.getElementById('app');
+
+ReactDOM.render(expresion, container);
