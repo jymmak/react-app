3c3c34ffb1a101578b0d8e0d1dd6e1bc14205af9
jymma karina mogollon cardoza <jkmogollon.cp@gmail.com>
2020-01-08 20:53:00 -0500

    add avatar

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -11,7 +11,7 @@ class Badge extends React.Component {
                     <img src={confLogo} alt="Logo de la conferencia" />
                 </div>
                 <div className="Badge__section-name">
-                    <img className="Badge__avatar" src={this.props.avatar} alt="Avatar" />
+                    <img className="Badge__avatar" src='https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon' alt="Avatar" />
                     <h1>{this.props.firstName} <br />{this.props.lastName}</h1>
 
                 </div>
