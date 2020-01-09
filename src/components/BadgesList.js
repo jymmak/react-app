55edb5aa2890c700029d0be2221f99505c8df81c
jymma karina mogollon cardoza <jkmogollon.cp@gmail.com>
2020-01-09 11:45:31 -0500

    calling api and simulation never badge

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import '../styles/BadgesList.css';
+import {Link}  from 'react-router-dom';
 
 class BadgesList extends React.Component {
     render() {
-        return (
+if(this.props.badges.length === 0){
+    return (
+        <div>
+            <h3>
+                No encontramos ningún badge
+            </h3>
 
+            <Link className="btn btn-primary" to="/badges/new">Create new badge</Link>
+        </div>
+    )
+}
+
+
+        return (
             <ul className="list-unstyled">
                 {this.props.badges.map(badge => {
                     return (
