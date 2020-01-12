85869903b0a830242996ae84150c98ec420a6594
jymma karina mogollon cardoza <jkmogollon.cp@gmail.com>
2020-01-11 22:57:26 -0500

    containers and components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Badges from './pages/Badges';
 import BadgeNew from './pages/BadgeNew';
 import BadgeEdit from './pages/BadgeEdit';
-import BadgeDetails from './pages/BadgeDetails';
+import BadgeDetails from './pages/BadgeDetailsContainer';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout';
 import NotFound from './pages/NotFound';
@@ -16,7 +16,7 @@ function App() {
           <Route exact path="/badges" component={Badges} />
           <Route exact path="/badges/new" component={BadgeNew} />
           <Route exact path="/badges/:badgeId" component={BadgeDetails} />
-          <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
+           <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
           <Route component={NotFound} />
         </Switch>
       </Layout>
--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -1,88 +1,62 @@
 import React from 'react';
-import '../styles/BadgeDetails.css'
-import confLogo from '../assets/images/platziconf-logo.svg'
-import PageLoading from '../components/PageLoading';
-import PageError from '../components/PageError';
-import api from '../api'
-import Badge from '../components/Badge'
 import { Link } from 'react-router-dom';
 
-class BadgeDetails extends React.Component {
+import '../styles/BadgeDetails.css';
+import confLogo from '../assets/images/platziconf-logo.svg';
+import Badge from '../components/Badge';
 
-    state = {
-        loading: true,
-        error: null,
-        data: undefined,
-    }
+function BadgeDetails(props) {
+  const badge = props.badge;
 
-    componentDidMount() {
-        this.fetchData();
-    }
-
-    fetchData = async () => {
-        this.setState({ loading: true, error: null });
-        try {
-            const data = await api.badges.read(
-                this.props.match.params.badgeId
-
-            )
-            this.setState({ loading: false, data: data });
-        } catch (error) {
-            this.setState({ loading: false, error: error })
-        }
-    }
-    render() {
-        if (this.state.loading === true) {
-            return <PageLoading />;
-
-        }
-        if (this.state.error) {
-            return <PageError error={this.state.error} />;
-        }
-
-        const badge = this.state.data;
-        return (
-            <div>
-                <div className="BadgeDetails__hero">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-sm-6">
-                                <img src={confLogo} alt="Logo de la conferencia" />
-                            </div>
-
-                            <div className="col-sm-6 BadgeDetails__hero-attendant-name ">
-                                <h1>{badge.firstName}&nbsp; {badge.lastName}</h1>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-
-                <div className="container">
-                    <div className="row">
-                        <div className="col-sm-6">
-                            <Badge firstName={badge.firstName} lastName={badge.lastName}
-                                email={badge.email} twitter={badge.twitter} jobTitle={badge.jobTitle} />
-                        </div>
-                        <div className="col-sm-6">
-                            <h2>Actions</h2>
-                            <div>
-                                <div>
-                                    <Link className="btn btn-primary mb-4" to={`/badges/${badge.id}/edit`}>Edit</Link>
-                                </div>
-                                <div>
-                                    <button className="btn btn-danger">Delete</button>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-
+  return (
+    <div>
+      <div className="BadgeDetails__hero">
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <img src={confLogo} alt="Logo de la Conferencia" />
             </div>
+            <div className="col-6 BadgeDetails__hero-attendant-name">
+              <h1>
+                {badge.firstName} {badge.lastName}
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
 
-        )
-    }
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Badge
+              firstName={badge.firstName}
+              lastName={badge.lastName}
+              email={badge.email}
+              twitter={badge.twitter}
+              jobTitle={badge.jobTitle}
+            />
+          </div>
+          <div className="col">
+            <h2>Actions</h2>
+            <div>
+              <div>
+                <Link
+                  className="btn btn-primary mb-4"
+                  to={`/badges/${badge.id}/edit`}
+                >
+                  Edit
+                </Link>
+              </div>
+
+              <div>
+                <button className="btn btn-danger">Delete</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BadgeDetails;
